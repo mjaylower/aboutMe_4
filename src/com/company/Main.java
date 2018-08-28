828c6fb3f21250e62ba4e202180003dabbb2665b
mjaylower <mlower@students.mchenry.edu>
2018-08-28 11:46:29 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello! My name is Michael Lower");
+        System.out.println("This is my 3rd semester taking online courses at MCC.");
+        System.out.println("I have thoroughly enjoyed learning more about various programming languages.");
+        System.out.println("I hope to use this information to further my career objectives.");
+        System.out.println("Currently I work for a software company, but would like to be more involved in the code side of things.");
+
+    }
+}
